6cf8d4b6feea242ccc127a7a2f87f6f6cd4c68e5
neo-level <info@neo-level.com>
2021-12-26 17:24:27 +0100

    created server side simulation

--- Assets/Scripts/TcpListenSocketBehaviour.cs
+++ Assets/Scripts/TcpListenSocketBehaviour.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using UnityEngine;
 
+/// <summary>
+/// Simulated server side.
+/// </summary>
 public class TcpListenSocketBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TcpListener _listener;
+
+    [HideInInspector] public bool isReady;
+
+    [Tooltip("The port the service is running on.")]
+    public int port = 9021;
+
+    private void Start()
     {
-        
+        _listener = new TcpListener(IPAddress.Any, port);
+        _listener.Start();
+        _listener.BeginAcceptSocket(Socket_Connected, _listener);
+        isReady = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        _listener?.Stop();
+        _listener = null;
+    }
+
+    private void Socket_Connected(IAsyncResult result)
+    {
+        if (result.IsCompleted)
+        {
+            var socket = (result.AsyncState as TcpListener)?.EndAcceptSocket(result);
+            int offset = 0;
+            var state = new StateObject(socket);
+            socket?.BeginReceive(state.Buffer, offset, state.Buffer.Length, SocketFlags.None, Socket_Received, state);
+        }
+    }
+
+    private void Socket_Received(IAsyncResult result)
+    {
+        if (result.IsCompleted)
+        {
+            var state = result.AsyncState as StateObject;
+            var bytesIn = state.Socket.EndReceive(result);
+            if (bytesIn > 0)
+            {
+                int index = 0;
+                var message = Encoding.ASCII.GetString(state.Buffer, index, bytesIn);
+                print($"from client: {message}");
+            }
+
+            int offset = 0;
+            var newState = new StateObject(state?.Socket);
+            state?.Socket.BeginReceive(state.Buffer, offset, state.Buffer.Length, SocketFlags.None, Socket_Received,
+                newState);
+        }
     }
 }
